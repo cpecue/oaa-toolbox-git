a183770fa4511b07cbe868b5d20b051cf1a838a3
cpecue <caleb.pecue.1@gmail.com>
2021-04-05 12:12:05 -0500

    Messing with Requirements.txt

--- oaatoolbox/__init__.py
+++ oaatoolbox/__init__.py
@@ -1,9 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import create_engine
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-import secrets
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
